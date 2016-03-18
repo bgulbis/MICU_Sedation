6a707a01954089982215b13c46ec06d8426a1d90
bgulbis <brian.gulbis@gmail.com>
2016-03-17 21:59:10 -0500

    output sedative results to Word

--- report.R
+++ report.R
@@ -4,6 +4,8 @@ source("library.R")
 
 library(tableone)
 library(ReporteRs)
+library(purrr)
+library(broom)
 
 # get data
 if (!exists("data.demograph")) data.demograph <- readRDS("Analysis/data_demograph.Rds")
@@ -11,22 +13,42 @@ if (!exists("data.assessments")) data.assessments <- readRDS("Analysis/data_asse
 if (!exists("data.lfts")) data.lfts <- readRDS("Analysis/data_lfts.Rds")
 if (!exists("data.sedatives")) data.sedatives <- readRDS("Analysis/data_sedatives.Rds")
 
+normal_test <- function(x) {
+    y <- keep(x, is.numeric)
+    map_df(y, possibly(shapiro.test, 
+                       otherwise = list(statistic = NA, p.value = NA, 
+                                        method = "Shapiro-Wilk normality test", 
+                                        data.name = ".x[[i]]"))) %>%
+        mutate(data.name = names(y)) %>%
+        select(data.name, method, statistic, p.value)
+}
+
+
 test <- data.demograph %>%
     mutate(group = ifelse(bzd == TRUE, "BZD", "No BZD")) %>%
     select(-pie.id, -bzd, -diagnosis)
 
 vars <- names(test)
+vars <- vars[vars != "group"]
 
 cont <- keep(test, is.numeric)
 contVars <- names(cont)
+
+# normality testing
+nrml <- normal_test(cont)
+
+not.nrml <- filter(nrml, p.value < 0.05)
+not.nrmlVars <- not.nrml$data.name
 
 cat <- discard(test, is.numeric)
 catVars <- names(cat)
 
 tab <- CreateTableOne(vars, strata = "group", data = test, factorVars = catVars)
-tab1 <- print(tab)
+tab1 <- print(tab, printToggle = FALSE, nonnormal = not.nrmlVars)
 # make Word document
 mydoc <- docx()
+styles(mydoc)
+mydoc <- declareTitlesStyles(mydoc, stylenames = c("Titre1", "Titre2", "rTableLegend"))
 
 # add table
 mydoc <- addTitle(mydoc, "Results", level = 3)
@@ -37,33 +59,40 @@ mytable <- setZebraStyle(mytable, odd = "#eeeeee", even = "white")
 
 mydoc <- addFlexTable(mydoc, mytable)
 
-# continous data
-mydoc <- addTitle(mydoc, "Detailed Continuous Data", level = 3)
-mytable <- FlexTable(print(summary(tab$ContTable)), add.rownames = TRUE, header.cell.props = cellProperties(background.color = "#003366"), 
+# sedatives ----
+
+sed <- data.sedatives %>% 
+    mutate(group = ifelse(bzd == TRUE, "BZD", "No BZD")) %>%
+    select(med, group, time.wt.avg.rate:total.dose) 
+    
+vars <- names(sed)
+vars <- vars[!(vars %in% c("med", "group"))]
+
+test <- sed %>%
+    select(-group) %>%
+    slice_rows("med") %>%
+    by_slice(normal_test)
+
+x <- NULL
+for(i in 1:length(test$med)) {
+    tbl <- filter(sed, med == test$med[[i]]) %>%
+        select(-med)
+    vars <- names(tbl)
+    vars <- vars[vars != "group"]
+    tbl1 <- CreateTableOne(vars, strata = "group", data = tbl)
+    y <- matrix(c("", "", "", ""), ncol = 4, dimnames = list(as.character(test$med[[i]])))
+    y <- rbind(y, print(tbl1, printToggle = FALSE, nonnormal = vars))
+    x <- rbind(x, y)
+}
+
+mydoc <- addParagraph(mydoc, "")
+
+mydoc <- addTitle(mydoc, "Sedatives", level = 3)
+mytable <- FlexTable(x, add.rownames = TRUE, header.cell.props = cellProperties(background.color = "#003366"), 
                      header.text.props = textBold(color = "white", font.family = "Calibri"))
 mytable[] <- textProperties(font.family = "Calibri")
 mytable <- setZebraStyle(mytable, odd = "#eeeeee", even = "white")
 
 mydoc <- addFlexTable(mydoc, mytable)
 
-
 writeDoc(mydoc, file = "Analysis/results.docx")
-# docx( ) %>% 
-#     addFlexTable( mtcars %>%
-#                       FlexTable( header.cell.props = cellProperties( background.color =  "#003366" ),
-#                                  header.text.props = textBold( color = "white" ),
-#                                  add.rownames = TRUE ) %>%
-#                       setZebraStyle( odd = "#DDDDDD", even = "#FFFFFF" ) ) %>%
-#     writeDoc( file = "exemple.docx" )
-
-
-# mydoc <- addTitle(mydoc, "Letter of Intent", level = 3)
-# mytable <- vanilla.table(loi.table)
-# mytable[, to = "header"] <- textProperties(font.size = 10, font.family = "Calibri", font.weight = "bold")
-# mytable[, to = "header"] <- parCenter()
-# mytable[] <- textProperties(font.size = 8, font.family = "Calibri")
-# mytable <- setFlexTableWidths(mytable, widths = c(1.5, 0.75, 5.25))
-# mytable[, 2] <- parCenter()
-# mytable[, 3] <- parLeft()
-# mytable <- setZebraStyle(mytable, odd = "#eeeeee", even = "white")
-# mydoc <- addFlexTable(mydoc, mytable)
